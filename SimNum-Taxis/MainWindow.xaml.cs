83f4e79f5823cb61bd025501c1a8228d2ee99f00
Petrolevb-Asus <Vincebellec@gmail.com>
2013-10-04 22:13:28 +0200

    Draw in the canvas

--- SimNum-Taxis/MainWindow.xaml.cs
+++ SimNum-Taxis/MainWindow.xaml.cs
@@ -25,11 +25,16 @@ namespace SimNum_Taxis
         public MainWindow()
         {
             InitializeComponent();
+            // Call the redraw function when the window size change
+            this.SizeChanged += new SizeChangedEventHandler((object o, SizeChangedEventArgs e) 
+                                    => { this.Dispatcher.Invoke((Action)(() 
+                                        => { this.ReDrawCanvas(); })); });
             this.m_Time = DateTime.Today;
             this.c_Time_TextBlock.Text = this.m_Time.Hour + "H" + this.m_Time.Minute;
 
             this.m_City = new City();
             this.m_City.TimeElapsed += (object o, System.Timers.ElapsedEventArgs e) =>
+            #region Time Elapsed Event
                 {
                     this.m_Time = this.m_Time.AddMinutes(this.m_City.RatioTime);
                     // Need to use the Dispatcher :  Allow a thread
@@ -40,24 +45,46 @@ namespace SimNum_Taxis
                             this.c_Time_TextBlock.Text = 
                                 String.Format("{0,2}H{1,2}",
                                     this.m_Time.Hour, this.m_Time.Minute);
+                            this.ReDrawCanvas();
                         }));
                 };
+            #endregion
             this.c_SizeCity_TextBlock.Text = this.m_City.SizeCity.ToString();
+
             this.m_City.SizeCityChanged += (object sender, EventArgs e) =>
+            #region Size City changed event
                 {
                     this.c_SizeCity_TextBlock.Dispatcher.Invoke((Action)(
                         () => { this.c_SizeCity_TextBlock.Text = this.m_City.SizeCity.ToString(); }));
+                    this.ReDrawCanvas();
                 };
+            #endregion
+
+            this.ReDrawCanvas();
         }
-        
+
+        private void ReDrawCanvas()
+        {
+            double height = this.c_City.ActualHeight,
+                   width = this.c_City.ActualWidth;
+            this.c_City.Children.Clear();
+            Ellipse el = new Ellipse()
+            {
+                Width = 25*this.m_City.SizeCity, Height = 25*this.m_City.SizeCity,
+                StrokeThickness = 2.0, Stroke = Brushes.Black
+            };
+
+            Canvas.SetTop(el, height/2.0 - el.Height/2.0);
+            Canvas.SetLeft(el, width/2.0- el.Width/2.0);
+            this.c_City.Children.Add(el);
+        }
+
+        #region Manage the Faster Time Button
         private void c_FasterTime_Button_Click(object sender, RoutedEventArgs e)
-        {
-            this.m_City.RatioTime *= 10;
-        }
+        { this.m_City.RatioTime *= 10; }
         private void c_FasterTime_Button_Released(object sender, RoutedEventArgs e)
-        {
-            this.m_City.RatioTime /= 10;
-        }
+        { this.m_City.RatioTime /= 10; }
+        #endregion
 
         #region Add or Remove a Taxi
         private void c_TaxisNumberMinus_Button_Click(object sender, RoutedEventArgs e)
